18d0c653c0de950f6eb53657ccb3d9b2d7fd661b
André Silva <andre15andre@hotmail.com>
2021-06-04 17:55:31 +0200

    Add another working class and trigger checkstyle again
    Signed-off-by: André Silva <andre15andre@hotmail.com>

---
+++ src/main/java/AnotherWorkingClass.java
@@ -0,0 +1,11 @@
+public class AnotherWorkingClass {
+
+    public WorkingClass() {
+        super();
+    }
+
+    public int getInt() {
+        return 1;
+    }
+
+}
--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -10,6 +10,6 @@ public class TestClass {
 		b = 1;
 	}
 
-	
+
 
 }
