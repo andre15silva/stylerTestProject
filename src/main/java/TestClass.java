d84e684a52325841727fbce8e20ebae0498e5d8c
André Silva <andre15andre@hotmail.com>
2021-06-15 11:04:12 +0200

    Fix checkstyle issues
    Signed-off-by: André Silva <andre15andre@hotmail.com>

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,18 +1,14 @@
-import java.lang
-.Exception;
+import java.lang.Exception;
 
 public class TestClass extends java.lang.Exception {
 
-	private int a;
+        private int a;
 
-	private int b;
+        private int b;
 
-	public TestClass()
-	{
-		a = 0;
-		b = 1;
-	}
-
-
+        public TestClass() {
+                a = 0;
+                b = 1;
+        }
 
 }
